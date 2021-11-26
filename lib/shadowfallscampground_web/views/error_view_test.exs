adac6d743b3443cec8d2f435210231aac1898433
Justin Morgan <justin.morgan@hey.com>
2021-11-25 20:19:58 -0800

    :truck:  Colocate tests to source files

--- lib/shadowfallscampground_web/views/error_view_test.exs
+++ lib/shadowfallscampground_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule ShadowfallscampgroundWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ShadowfallscampgroundWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ShadowfallscampgroundWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
