83a244d6eb358ea1668d950473634f79e23b7f5c
Justin Morgan <justin.morgan@hey.com>
2022-01-06 16:21:28 -0800

    :heavy_plus_sign: (tailwind) add typography and forms plugins

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,12 +1,24 @@
+const colors = require("tailwindcss/colors");
+const typography = require("@tailwindcss/typography");
+const forms = require("@tailwindcss/forms");
+
 module.exports = {
   mode: "jit",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex", "../lib/*_web/**/*.sface"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.green,
+        accent: colors.purple,
+      },
+      backgroundImage: {
+        trees: "url(/images/pexels-brandon-montrone-1179229--narrow_1200.jpg)",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography, forms],
 };
