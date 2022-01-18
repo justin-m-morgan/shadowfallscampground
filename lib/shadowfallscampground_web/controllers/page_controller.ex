d085fe1b4245625056ee922dd60f2a365d506ae9
Justin Morgan <justin.morgan@hey.com>
2022-01-18 10:43:58 -0800

    :fire:  Remove default generated page controller

--- lib/shadowfallscampground_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule ShadowfallscampgroundWeb.PageController do
-  use ShadowfallscampgroundWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/shadowfallscampground_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ShadowfallscampgroundWeb.PageControllerTest do
-  use ShadowfallscampgroundWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Shadowfalls"
-  end
-end
