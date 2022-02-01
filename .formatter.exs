05193465a752e9620778106d7aa790c824d6d970
Justin Morgan <justin.morgan@hey.com>
2022-02-01 10:22:06 -0800

    :wrench:  Change formatting config to incorporate surface formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 [
   import_deps: [:ecto, :phoenix, :surface],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [Surface.Formatter.Plugin],
   subdirectories: ["priv/*/migrations"],
   surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"]
 ]
