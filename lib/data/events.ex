9e86ba2f4182ea54e3b3d8544a8b717650412326
Justin Morgan <justin.morgan@hey.com>
2022-03-04 10:23:18 -0800

    :speech_balloon: (events) Remove easter for 2022

--- lib/data/events.ex
+++ lib/data/events.ex
@@ -6,18 +6,25 @@ defmodule Shadowfallscampground.Data.Events do
 
   def events_listing() do
     [
+      # %__MODULE__{
+      #   name: "Easter",
+      #   theme: "Unofficial Season Opening",
+      #   date: NaiveDateTime.new!(2022, 4, 15, 21, 0, 0),
+      #   description:
+      #     "Just an early opportunity to air out the camping gear. The campground will only be open on weekends until May 13th",
+      #   icon: "easter"
+      # },
       %__MODULE__{
-        name: "Easter",
-        theme: "Unofficial Season Opening",
-        date: NaiveDateTime.new!(2022, 4, 15, 21, 0, 0),
-        description:
-          "Just an early opportunity to air out the camping gear. The campground will only be open on weekends until May 13th",
-        icon: "easter"
+        name: "Season Opening",
+        theme: "No Event",
+        date: Date.new!(2022, 5, 13),
+        description: "Opening weekend. ",
+        icon: "hand"
       },
       %__MODULE__{
         name: "Victoria Day",
         theme: "Welcome Back BBQ",
-        date: NaiveDateTime.new!(2022, 5, 21, 21, 0, 0),
+        date: Date.new!(2022, 5, 21),
         description:
           "Join your fellow campers for a burger and smokey at the clubhouse on Saturday afternoon.",
         icon: "burger"
@@ -25,7 +32,7 @@ defmodule Shadowfallscampground.Data.Events do
       %__MODULE__{
         name: "Canada Day",
         theme: "Disco Dance",
-        date: NaiveDateTime.new!(2022, 7, 2, 21, 0, 0),
+        date: Date.new!(2022, 7, 2),
         description:
           "Dust off your dancing shoes and iron your bell-bottoms. And don't miss out on the 'More Fun' Punch",
         icon: "disco"
@@ -33,7 +40,7 @@ defmodule Shadowfallscampground.Data.Events do
       %__MODULE__{
         name: "BC Day",
         theme: "Chilli Cookoff",
-        date: NaiveDateTime.new!(2022, 7, 30, 21, 0, 0),
+        date: Date.new!(2022, 7, 30),
         description:
           "All campers are invited to join the competition for a spot on the coveted 'Plaque of Chilli Champions'.",
         icon: "cookpot"
@@ -41,14 +48,14 @@ defmodule Shadowfallscampground.Data.Events do
       %__MODULE__{
         name: "Labour Day",
         theme: "Pig Roast",
-        date: NaiveDateTime.new!(2022, 9, 3, 21, 0, 0),
+        date: Date.new!(2022, 9, 3),
         description: "Watch the pig go round and round all day until dinner time.",
         icon: "pig"
       },
       %__MODULE__{
         name: "Season Close",
         theme: "No Event",
-        date: NaiveDateTime.new!(2022, 9, 15, 21, 0, 0),
+        date: Date.new!(2022, 9, 15),
         description:
           "Last call for camping. We may stay open later than this if there is continued demand and the weather cooperates.",
         icon: "hand"
