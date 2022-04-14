03cf9a2fb811db54b1f5d278689fb0713a040860
Justin Morgan <justin.morgan@hey.com>
2022-04-14 16:45:37 -0700

    :speech_balloon: (reservation-form) Update lottery dates

--- lib/shadowfallscampground_web/live/reserve_live/lottery_manager.ex
+++ lib/shadowfallscampground_web/live/reserve_live/lottery_manager.ex
@@ -8,9 +8,9 @@ defmodule ShadowfallscampgroundWeb.ReserveLive.LotteryManager do
   prop now, :struct
   prop season_start, :struct, default: ~D[2022-05-12]
   prop season_end, :struct, default: ~D[2022-09-14]
-  prop lottery_open, :struct, default: ~D[2022-04-07]
-  prop lottery_close, :struct, default: ~N[2022-04-08 23:59:00]
-  prop lottery_period_end_date, :struct, default: ~D[2022-06-22]
+  prop lottery_open, :struct, default: ~D[2022-04-14]
+  prop lottery_close, :struct, default: ~N[2022-04-15 23:59:00]
+  prop lottery_period_end_date, :struct, default: ~D[2022-07-13]
 
   # prop lottery_dates,
   #      :list,
